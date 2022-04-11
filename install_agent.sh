d51936f3d926c96a244f80bb381b0ad53c80179b
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2022-04-11 11:08:09 +0300

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -49,8 +49,7 @@ sed -i "s/ServerActive=127.0.0.1/ServerActive=zabbix1.sys.ilogy.ru:57799/" /etc/
 echo ""
 echo ""
 echo  "Введите имя этого хоста для Zabbix сервера - не обязательно, чтобы оно совпадало с реальным хостнеймом"
-echo -n "Это имя используйте при создании узла сети в Zabbix сервере:  "
-read 
+read -p "Это имя используйте при создании узла сети в Zabbix сервере:  "
 echo ""
 echo ""
 sed -i "s/Hostname=Zabbix server/Hostname=${REPLY}/" /etc/zabbix/zabbix_agentd.conf
