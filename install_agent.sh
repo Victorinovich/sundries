0a348984ea2019f259c898005a774eac5750d51c
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2022-04-12 13:15:58 +0300

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -1,58 +1,67 @@
 #!/bin/bash
-
 apt purge zabbix-agent zabbix-release -y
-
 DISTR=`lsb_release -i  | awk '{print $3}'`
 RELEASE=`lsb_release -r  | awk '{print $2}'`
-
-if [[ $DISTR == "Debian" ]]
-then
-
-case "$RELEASE" in
-
-"9")
-wget http://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian9_all.deb
-dpkg -i zabbix-release_6.0-1+debian9_all.deb
-apt update
-apt install zabbix-agent -y
-;;
-
-"10")
-wget http://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian10_all.deb
-dpkg -i zabbix-release_6.0-1+debian10_all.deb
-apt update
-apt install zabbix-agent -y
-;;
-
-"11")
-wget http://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian11_all.deb
-dpkg -i zabbix-release_6.0-1+debian11_all.deb
-apt update
-apt install zabbix-agent -y
-;;
-
-*) 
-echo "Дистрибутив не поддерживается"
-exit
-;;
-
-esac
-
-else
-echo "Дистрибутив не поддерживается"
-exit
-fi
-
+   if [[ $DISTR == "Debian" ]]
+   then
+         case "$RELEASE" in
+         "9")
+         wget http://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian9_all.deb
+         dpkg -i zabbix-release_6.0-1+debian9_all.deb
+         apt update
+         apt install zabbix-agent -y
+         ;;
+         "10")
+         wget http://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian10_all.deb
+         dpkg -i zabbix-release_6.0-1+debian10_all.deb
+         apt update
+         apt install zabbix-agent -y
+         ;;
+         "11")
+         wget http://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian11_all.deb
+         dpkg -i zabbix-release_6.0-1+debian11_all.deb
+         apt update
+         apt install zabbix-agent -y
+         ;;
+         *) 
+         echo "Дистрибутив не поддерживается"
+         exit
+         ;;
+         esac
+   else
+     echo "Дистрибутив не поддерживается"
+   exit
+   fi
 sed -i "s/Server=127.0.0.1/Server=zabbix1.sys.ilogy.ru/" /etc/zabbix/zabbix_agentd.conf
 sed -i "s/ServerActive=127.0.0.1/ServerActive=zabbix1.sys.ilogy.ru:57799/" /etc/zabbix/zabbix_agentd.conf
-
-echo ""
 echo ""
 echo  "Введите имя этого хоста для Zabbix сервера - не обязательно, чтобы оно совпадало с реальным хостнеймом"
 read -p "Это имя используйте при создании узла сети в Zabbix сервере: "
-echo ""
 echo ""
 sed -i "s/Hostname=Zabbix server/Hostname=${REPLY}/" /etc/zabbix/zabbix_agentd.conf
 systemctl start zabbix-agent
 systemctl enable zabbix-agent
 rm -f zabbix-release*.deb*
+
+echo ""
+echo "Сервер виртуальный? (y/n)"
+select yn in "yes" "no"
+do
+case $yn in
+yes ) 
+exit
+;;
+no ) 
+echo "UserParameter=mdadm.status, egrep -c \"\[.*_.*\]\" /proc/mdstat" >> /etc/zabbix/zabbix_agentd.d/userparameters_mdadm.conf
+apt install smartmontools -y
+chmod u+s /usr/sbin/smartctl
+echo "UserParameter=storage.discovery[*], /usr/local/bin/smartctl-storage-discovery.sh
+UserParameter=storage.get[*],if [ -n \"\$1\" ]; then /usr/sbin/smartctl -i -H -A -l error -l background \$1; fi
+UserParameter=smartctl.version,/usr/sbin/smartctl --version | grep -Eo \"^smartctl\s[0-9\.[:space:]\r-]+\" | sed -e 's/^smartctl.//'" >> /etc/zabbix/zabbix_agentd.d/userparameters_smartmontools.conf
+curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/smartctl-storage-discovery.sh > /usr/local/bin/smartctl-storage-discovery.sh
+chmod +x /usr/local/bin/smartctl-storage-discovery.sh
+;;
+esac
+done
+
+systemctl restart zabbix-agent
