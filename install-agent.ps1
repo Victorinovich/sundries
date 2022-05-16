3a67f919ab2f61f30052310c8b2dcb04888aede4
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2022-05-16 21:47:43 +0300

    Update install-agent.ps1

--- install-agent.ps1
+++ install-agent.ps1
@@ -1,6 +1,5 @@
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.4/zabbix_agent-6.0.4-windows-amd64-openssl.msi", "$env:USERPROFILE\Downloads\zabbix_agent-6.0.4-windows-amd64-openssl.msi")
-[Console]::outputEncoding = [System.Text.Encoding]::GetEncoding('UTF8')
 write-host "Введите имя хоста"
 $hostname=read-host
 $MYMSI="$env:USERPROFILE\Downloads\zabbix_agent-6.0.4-windows-amd64-openssl.msi"
