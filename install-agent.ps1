3e656da36248aa5ae027fc20570aa86e31623a7d
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2022-05-16 21:27:42 +0300

    Update install-agent.ps1

--- install-agent.ps1
+++ install-agent.ps1
@@ -1,7 +1,9 @@
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.4/zabbix_agent-6.0.4-windows-amd64-openssl.msi", "$env:USERPROFILE\Downloads\zabbix_agent-6.0.4-windows-amd64-openssl.msi")
 
-write-host "Ведите имя узла: "
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
+
+write-host "Vedite imya hosta: "
 $hostname=read-host
 
 $MYMSI="$env:USERPROFILE\Downloads\zabbix_agent-6.0.4-windows-amd64-openssl.msi"
