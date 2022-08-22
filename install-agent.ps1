c35daa760986181129ca3dd7f5900914863a212b
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2022-08-22 11:50:46 +0300

    Update install-agent.ps1

--- install-agent.ps1
+++ install-agent.ps1
@@ -1,3 +1,4 @@
+ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://cdn.zabbix.com/zabbix/binaries/stable/6.2/6.2.1/zabbix_agent-6.2.1-windows-amd64-openssl.msi", "$env:USERPROFILE\Downloads\zabbix_agent-6.2.1-windows-amd64-openssl.msi")
 write-host "Введите имя хоста - не обязательно, чтобы оно совпадало с реальным хостнеймом. Это имя используйте при создании узла сети на сервере Zabbix"
