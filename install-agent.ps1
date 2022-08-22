528473b02d4935d84fb62c596938a7c9cdb2aec5
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2022-08-22 11:35:25 +0300

    Update install-agent.ps1

--- install-agent.ps1
+++ install-agent.ps1
@@ -1,6 +1,5 @@
-$client = new-object System.Net.WebClient
 [Net.ServicePointManager]::SecurityProtocol +='tls12'
-Invoke-WebRequest -Uri 'https://cdn.zabbix.com'
+$client = new-object System.Net.WebClient
 $client.DownloadFile("https://cdn.zabbix.com/zabbix/binaries/stable/6.2/6.2.1/zabbix_agent-6.2.1-windows-amd64-openssl.msi", "$env:USERPROFILE\Downloads\zabbix_agent-6.2.1-windows-amd64-openssl.msi")
 write-host "Введите имя хоста - не обязательно, чтобы оно совпадало с реальным хостнеймом. Это имя используйте при создании узла сети на сервере Zabbix"
 $hostname=read-host
