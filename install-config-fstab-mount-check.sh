f2b735cb923edcc0e0dc2604a826af47a9625e07
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2022-12-27 10:41:25 +0300

    Update install-config-fstab-mount-check.sh

--- install-config-fstab-mount-check.sh
+++ install-config-fstab-mount-check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "UserParameter=fstab.mount.check.status, if test "$(mount -a)"; then echo "1"; else echo "0"; fi"  > /etc/zabbix/zabbix_agentd.d/userparameters_mount_check.conf
+echo 'UserParameter=fstab.mount.check.status, if test "$(mount -a)"; then echo "1"; else echo "0"; fi'  > /etc/zabbix/zabbix_agentd.d/userparameters_mount_check.conf
