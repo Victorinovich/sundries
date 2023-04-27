f30e36504825fd32be5d2b59063749c2f23de4d5
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2023-04-27 10:34:19 +0300

    Update install_iostat_template_parameters.sh

--- install_iostat_template_parameters.sh
+++ install_iostat_template_parameters.sh
@@ -3,8 +3,7 @@
 apt update
 apt install sysstat -y
 
-echo "* * * * * root DATA=$(S_TIME_FORMAT=ISO /usr/bin/iostat -yxdt -o JSON 59 1) ; echo \"$DATA\" >/tmp/iostat-cron.out
-" > /etc/cron.d/iostat
+curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/crontab > /etc/cron.d/iostat
 
 curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/iostat.conf > /etc/zabbix/zabbix_agentd.d/iostat.conf
 #sleep 3
