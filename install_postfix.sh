65bcbf66f649432a25547fe1e681a00610749f2e
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2023-05-22 14:44:45 +0300

    Add files via upload

---
+++ install_postfix.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+apt update
+echo "postfix postfix/mailname string $HOSTNAME.local" | debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Satellite system'" | debconf-set-selections
+apt install postfix mailutils -y
+
+echo ""
+echo -n "Введите имя для почтового поля FROM - отправителя, от которого будут отсылаться письма, допустима только латиница:  "
+read name
+chfn -f "$name" root
+
+# Postfix on a null client
+cp /etc/postfix/main.cf /etc/postfix/main.cf.original
+echo "myhostname=$(cat /etc/mailname)
+mydestination = 
+relayhost =
+inet_interfaces = loopback-only
+" > /etc/postfix/main.cf
+
+echo ""
+echo -n "Отправить тестовое письмо? (y/n):  "
+read DANET
+if [[ $DANET == "y" || $DANET == "yes" ]]; then
+	echo ""
+	echo -n "Введите e-mail:  "
+	read ADDRESS
+	echo ""
+	echo "Test" | mail -s "Test" $ADDRESS
+	echo ""
+	echo "Письмо отправлено на $ADDRESS"
+	echo ""
+fi
