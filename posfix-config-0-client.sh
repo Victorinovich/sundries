58f30a2731fb108d6a5a742f8f82a4b6a741e26f
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2023-05-24 10:06:43 +0300

    Update posfix-config-0-client.sh

--- posfix-config-0-client.sh
+++ posfix-config-0-client.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mv /etc/postfix /etc/postfix.$(date "+%F--%H-%M-%S")
+mkdir /etc/postfix
 dpkg-reconfigure -f noninteractive postfix
 
 echo "Введите имя для почтового поля FROM - отправителя, от которого будут отсылаться письма, допустима только латиница"
