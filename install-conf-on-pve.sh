004570b2d9576bf6756d1ef16dfe083501542693
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2023-11-24 17:48:12 +0300

    Update install-conf-on-pve.sh

--- install-conf-on-pve.sh
+++ install-conf-on-pve.sh
@@ -7,7 +7,6 @@ echo "acl:1:/:@zabbixAPI:zabbix_API_monitoring:" >> /etc/pve/user.cfg
 #curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/proxmox-localstorage-status-discovery.sh > /etc/zabbix/zabbix_agentd.d/proxmox-localstorage-status-discovery.sh
 #curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/pbs-verify-backup-discovery.sh > /etc/zabbix/zabbix_agentd.d/pbs-verify-backup-discovery.sh
 #curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/userparameters_pveapi_scripts.conf > /etc/zabbix/zabbix_agentd.d/userparameters_pveapi_scripts.conf
-
+#chmod +x /etc/zabbix/zabbix_agentd.d/*.sh
 #sleep 3
-systemctl restart cron
-systemctl restart zabbix-agent
+#systemctl restart zabbix-agent
