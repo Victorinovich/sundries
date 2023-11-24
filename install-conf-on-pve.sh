48f2535ba4ac82ca5b8197f30df05baef301061e
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2023-11-24 17:45:26 +0300

    Update install-conf-on-pve.sh

--- install-conf-on-pve.sh
+++ install-conf-on-pve.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+echo "acl:1:/:@zabbixAPI:zabbix_API_monitoring:" >> /etc/pve/user.cfg
 
-apt update
-apt install sysstat -y
+#curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/proxmox-backup-discovery.sh > /etc/zabbix/zabbix_agentd.d/proxmox-backup-discovery.sh
+#curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/check-running-machines-to-enable-backup.sh > /etc/zabbix/zabbix_agentd.d/check-running-machines-to-enable-backup.sh
+#curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/check-machines-to-type-backup.sh > /etc/zabbix/zabbix_agentd.d/check-machines-to-type-backup.sh
+#curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/proxmox-localstorage-status-discovery.sh > /etc/zabbix/zabbix_agentd.d/proxmox-localstorage-status-discovery.sh
+#curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/pbs-verify-backup-discovery.sh > /etc/zabbix/zabbix_agentd.d/pbs-verify-backup-discovery.sh
+#curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/userparameters_pveapi_scripts.conf > /etc/zabbix/zabbix_agentd.d/userparameters_pveapi_scripts.conf
 
-curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/crontab > /etc/cron.d/iostat
-
-curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/iostat.conf > /etc/zabbix/zabbix_agentd.d/iostat.conf
 #sleep 3
 systemctl restart cron
 systemctl restart zabbix-agent
