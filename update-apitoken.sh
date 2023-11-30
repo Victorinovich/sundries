07acfa96b88c7369180d5b66c2cd6dbf4a0d3c9c
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2023-11-30 16:07:35 +0300

    Update update-apitoken.sh

--- update-apitoken.sh
+++ update-apitoken.sh
@@ -5,5 +5,5 @@ pveum user token add zabbixAPI@pve zabbix --privsep 0 > token.temp
 var1=`cat ./token.temp | grep tokenid | awk '{print $4}'`
 var2=`cat ./token.temp | grep value | tail -n1 | awk '{print $4}'`
 APITOKEN=`echo -n $var1"="$var2`
-sed -i 's/^APITOKEN=*/APITOKEN='"$APITOKEN"'/' /etc/zabbix/zabbix_agentd.d/*.sh
+sed -i 's/^APITOKEN=.*/APITOKEN='"$APITOKEN"'/' /etc/zabbix/zabbix_agentd.d/*.sh
 rm ./token.temp
