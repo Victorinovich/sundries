e00e10348875473e8c44663d764b6a3edc3667fb
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2024-03-15 17:50:34 +0300

    Update update-conf-on-pve.sh

--- update-conf-on-pve.sh
+++ update-conf-on-pve.sh
@@ -9,4 +9,12 @@ curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/pbs-verif
 curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/quantity-of-backups-discovery.sh > /etc/zabbix/zabbix_agentd.d/quantity-of-backups-discovery.sh
 curl -Ls https://raw.githubusercontent.com/Victorinovich/sundries/main/userparameters_pveapi_scripts.conf > /etc/zabbix/zabbix_agentd.d/userparameters_pveapi_scripts.conf
 chmod +x /etc/zabbix/zabbix_agentd.d/*.sh
+pveum user token remove zabbixAPI@pve zabbix
+pveum user token add zabbixAPI@pve zabbix --privsep 0 > /tmp/token.temp
+var1=`cat /tmp/token.temp | grep tokenid | awk '{print $4}'`
+var2=`cat /tmp/token.temp | grep value | tail -n1 | awk '{print $4}'`
+APITOKEN=`echo -n $var1"="$var2`
+echo "APITOKEN=$APITOKEN" > /etc/zabbix/zabbix_agentd.d/token.txt
+rm -f /tmp/token.temp
+
 systemctl restart zabbix-agent
