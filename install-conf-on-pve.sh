a9da18ee02387311d18469209f3669ce1be31049
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2024-03-19 13:57:14 +0300

    Update install-conf-on-pve.sh

--- install-conf-on-pve.sh
+++ install-conf-on-pve.sh
@@ -20,8 +20,9 @@ pveum user modify zabbixAPI@pve -group zabbixAPI
 pveum user token add zabbixAPI@pve zabbix --privsep 0 > /tmp/token.temp
 var1=`cat /tmp/token.temp | grep tokenid | awk '{print $4}'`
 var2=`cat /tmp/token.temp | grep value | tail -n1 | awk '{print $4}'`
-APITOKEN=`echo -n $var1"="$var2`
-echo "$APITOKEN" > /etc/zabbix/zabbix_agentd.d/token.txt
+#APITOKEN=`echo -n $var1"="$var2`
+#echo "$APITOKEN" > /etc/zabbix/zabbix_agentd.d/token.txt
+echo -n $var1"="$var2 > /etc/zabbix/zabbix_agentd.d/token.txt
 rm -f /tmp/token.temp
 sleep 3
 systemctl restart zabbix-agent
