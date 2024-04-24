4b7c1f3b7090863c4421986b6f457eaf8b02da37
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2024-04-24 11:28:32 +0300

    Delete install-config-fstab-mount-check.sh

--- install-config-fstab-mount-check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'UserParameter=fstab.mount.check.status, if test "$(findmnt -x 2>&1 | grep -E "no errors|0 errors")"; then echo "0"; else echo "1"; fi'  > /etc/zabbix/zabbix_agentd.d/userparameters_mount_check.conf
-systemctl restart zabbix-agent
