743c4b26c515147356661fbb055da7e06847a494
Victorinovich <51355299+Victorinovich@users.noreply.github.com>
2024-04-24 11:05:56 +0300

    Delete update-apitoken.sh

--- update-apitoken.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pveum user token remove zabbixAPI@pve zabbix
-pveum user token add zabbixAPI@pve zabbix --privsep 0 > /tmp/token.temp
-var1=`cat /tmp/token.temp | grep tokenid | awk '{print $4}'`
-var2=`cat /tmp/token.temp | grep value | tail -n1 | awk '{print $4}'`
-echo -n $var1"="$var2 > /etc/zabbix/zabbix_agentd.d/token.txt
-rm -f /tmp/token.temp
